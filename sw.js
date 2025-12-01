0c0b1a691b79f9a2ba355af67252dbd06e7e2539
MarkRustenholtz <markrusten@gmail.com>
2025-12-01 04:20:39 +0100

    Upgrade cache version from v6 to v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cr-gend-cache-v6";
+const CACHE_NAME = "cr-gend-cache-v7";
 const urlsToCache = [
   "./",               // page principale
   "./index.html",
@@ -42,6 +42,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
